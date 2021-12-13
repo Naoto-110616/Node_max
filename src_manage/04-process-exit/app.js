0add9122af5ae417cee86a49a3060f3d0ad7a7f9
Naoto-110616 <siodeitadaku@gmail.com>
2021-12-13 22:28:16 +0900

    response html

--- src_manage/04-process-exit/app.js
+++ src_manage/04-process-exit/app.js
@@ -1,7 +1,8 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	console.log(req.url, req.method, req.headers);
+	console.log(req);
+	process.exit();
 });
 
 server.listen(3004);
